5c0dcd45756fb4821a873c27d4b01a4a3444cc1d
Xaleros <remodeat@gmx.com>
2019-02-19 16:54:50 -0500

    Switch license from GPLv3 to AGPLv3

--- lucc.cpp
+++ lucc.cpp
@@ -1,27 +1,20 @@
-/*========================================================================*\
-|*  This file is part of lucc.                                            *|
-|*                                                                        *|
-|*  lucc is free software: you can redistribute it and/or modify          *|
-|*  it under the terms of the GNU General Public License as published by  *|
-|*  the Free Software Foundation, either version 3 of the License, or     *|
-|*  (at your option) any later version.                                   *|
-|*                                                                        *|
-|*  lucc is distributed in the hope that it will be useful,               *|
-|*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *|
-|*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *|
-|*  GNU General Public License for more details.                          *|
-|*                                                                        *|
-|*  You should have received a copy of the GNU General Public License     *|
-|*  along with lucc.  If not, see <http://www.gnu.org/licenses/>.         *|
-|*                                                                        *|
-\*========================================================================*/
-
-/*========================================================================
- * lucc.cpp - libunr implemented UCC
- * 
- * written by Adam 'Xaleros' Smith
- *========================================================================
-*/
+/*===========================================================================*\
+|*  lucc - An open source UCC implementation that makes use of libunr        *|
+|*  Copyright (C) 2018-2019  Adam W.E. Smith                                 *|
+|*                                                                           *|
+|*  This program is free software: you can redistribute it and/or modify     *|
+|*  it under the terms of the GNU Affero General Public License as           *|
+|*  published by the Free Software Foundation, either version 3 of the       *|
+|*  License, or (at your option) any later version.                          *|
+|*                                                                           *|
+|*  This program is distributed in the hope that it will be useful,          *|
+|*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *|
+|*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *|
+|*  GNU Affero General Public License for more details.                      *|
+|*                                                                           *|
+|*  You should have received a copy of the GNU Affero General Public License *|
+|*  along with this program.  If not, see <https://www.gnu.org/licenses/>.   *|
+\*===========================================================================*/
 
 // TODO: Implement commandlets
 //
